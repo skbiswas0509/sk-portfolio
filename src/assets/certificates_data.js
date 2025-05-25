8cd3436d95bde72b09fd4dafc0cd93bacd0f750b
skbiswas0509 <sukumarbsws98@gmail.com>
2025-05-25 15:27:43 +0600

    just a little update.

--- src/assets/certificates_data.js
+++ src/assets/certificates_data.js
@@ -1,8 +1,23 @@
 const certificates_data = [
     {
-        c_name: "Ethical Hacker",
-        c_auth: "Cisco",
-        c_link: "https://www.credly.com/badges/1d7aac3d-890f-4c03-b3e8-4d9e31096a23/linked_in_profile"
+        c_name: "Machine Learning Specialization",
+        c_auth: "DeepLearning.AI",
+        c_link: "https://www.coursera.org/account/accomplishments/specialization/CZF129G0S879"
+    },
+    {
+        c_name: "Google Cybersecurity Specialization",
+        c_auth: "Coursera",
+        c_link: "https://www.coursera.org/account/accomplishments/specialization/SEK4U8WWEL9X"
+    },
+    {
+        c_name: "Certified in Cybersecurity",
+        c_auth: "ISC2",
+        c_link: "https://www.linkedin.com/in/sukumar-biswas-919888244/details/certifications/1729663203268/single-media-viewer/?type=DOCUMENT&profileId=ACoAADykYyUBCX19rfDs0SNxlPDszvJSZLyXsKA"
+    },
+    {
+        c_name: "Web Fundamentals Certificate",
+        c_auth: "TryHackMe",
+        c_link: "https://tryhackme-certificates.s3-eu-west-1.amazonaws.com/THM-JIL7PET5HM.pdf"
     },
     {
         c_name: "Junior Cybersecurity Analyst",
@@ -10,9 +25,19 @@ const certificates_data = [
         c_link: "https://www.credly.com/badges/81c2ade5-81fc-41f0-bb38-adf84761f91b/linked_in_profile"
     },
     {
-        c_name: "Certified in Cybersecurity",
-        c_auth: "ISC2",
-        c_link: "https://www.linkedin.com/in/sukumar-biswas-919888244/details/certifications/1729663203268/single-media-viewer/?type=DOCUMENT&profileId=ACoAADykYyUBCX19rfDs0SNxlPDszvJSZLyXsKA"
+        c_name: "Ethical Hacker",
+        c_auth: "Cisco",
+        c_link: "https://www.credly.com/badges/1d7aac3d-890f-4c03-b3e8-4d9e31096a23/linked_in_profile"
+    },
+    {
+        c_name: "CompTIA Pentest Certificate",
+        c_auth: "TryHackMe",
+        c_link: "https://tryhackme-certificates.s3-eu-west-1.amazonaws.com/THM-U3DQBDZWTO.pdf"
+    },
+    {
+        c_name: "Jr Penetration Tester Certificate",
+        c_auth: "TryHackMe",
+        c_link: "https://tryhackme-certificates.s3-eu-west-1.amazonaws.com/THM-LNCSNAEGNC.pdf"
     },
     {
         c_name: "DevSecOps Certificate",
@@ -23,12 +48,7 @@ const certificates_data = [
         c_name: "Security Engineer",
         c_auth: "TryHackMe",
         c_link: "https://tryhackme-certificates.s3-eu-west-1.amazonaws.com/THM-CCTN8ZOGXZ.pdf"
-    },
-    {
-        c_name: "Machine Learning Specialization",
-        c_auth: "DeepLearning.AI",
-        c_link: "https://www.coursera.org/account/accomplishments/specialization/CZF129G0S879"
-    },
+    }
 ];
 
 export default certificates_data; 
