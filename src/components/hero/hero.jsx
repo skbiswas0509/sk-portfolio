a6f103fc96adaf0a5bc7173c22741a9f59823960
Sukumar Biswas <sukumarbsws98@gmail.com>
2026-02-12 21:32:34 +0600

    a little fix

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./hero.css"
-import profile_img from '../../assets/profile_img.jpg'
+import profile_img from '../../assets/sk.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = () => {
