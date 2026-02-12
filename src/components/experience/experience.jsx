d48ec1f3f953c75c96503e22f90b83b78113d97e
Sukumar Biswas <sukumarbsws98@gmail.com>
2026-02-12 20:26:45 +0600

    Added Experience section

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -3,24 +3,26 @@ import './experience.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 // import nsu_pic from '../../assets/nsu.jpg'
 
-const Education = () => {
+const Experience = () => {
   return (
-    <div id='education' className='edu'>
-        <div className='edu-title'>
+    <div id='education' className='exp'>
+        <div className='exp-title'>
           <h1>Experience</h1>
           <img src={theme_pattern} alt="Enter the theme pattern here" />
         </div>
 
-        <div className="education-info">
-          <div className="education-left">
-            {/* <img src={nsu_pic} alt="put the nsu pic here" /> */}
-          </div>
-          <div className="education-right">
-            <div className="education-para">
+        <div className="experience-info">
+          <div className="experience-left">
             <p className='p1'>ACNABIN Chartered Accountants</p>
-            <p>Information System Audit</p>
-            <p>Internship</p>
-            <p>2025 -2026</p>
+            <p>Information System Audit Internship</p>
+            <p>2025 - 2026</p>
+          </div>
+          <div className="experience-right">
+            <div className="experience-para">
+              <p>Assisted in auditing IT infrastructure, focusing on security compliance, access control, and data integrity.</p>
+              <p>Reviewed compliance with regulations (e.g., ISO, GDPR) and company policies, contributing to risk assessment reports.</p>
+              <p>Reviewed software licenses, asset inventory, password policies, and secure coding practices for web and mobile applications.</p>
+              <p>Prepared and maintained detailed audit workpapers, ensuring accuracy and adherence to professional standards.</p>
             </div>
           </div>
         </div>
@@ -28,4 +30,4 @@ const Education = () => {
   )
 }
 
-export default Education
+export default Experience
